fa04bb4b450e74187f7984d143d3ebc257ecdcf7
Grishma Shrestha <grishma@revol-tech.com.np>
2012-06-27 22:50:00 +0545

    changes

--- photo.js
+++ photo.js
@@ -3,6 +3,6 @@ $(document).ready(function()
   $("img").click(function()
   {
     var image = this.src;
-     $.facebox('<img src="'+image+'" class="pics"/><form id=comment><textarea name="comment"></textarea><br/><input type="submit" value="submit"/></form>');
+     $.facebox('<img src="'+image+'" width="400px" height="300px"/><form id=comment><textarea name="comment"></textarea><br/><input type="submit" value="submit"/></form>');
    });
 });
